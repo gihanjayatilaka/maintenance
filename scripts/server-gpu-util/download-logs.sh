1eaa67e0b3891bc785ac8b63402c46f2d79e7586
gihanjayatilaka <gihan@umd.edu>
2023-05-08 02:22:55 -0400

    update

--- scripts/server-gpu-util/download-logs.sh
+++ scripts/server-gpu-util/download-logs.sh
@@ -1,4 +1,5 @@
-scp -J e14158@tesla.ce.pdn.ac.lk localturinggihan@turing.ce.pdn.ac.lk:/storage/scratch/e14-4yp-low-light-images/logging/turing/* ../../reports/server-gpu-util/logs/;
+# scp -J e14158@tesla.ce.pdn.ac.lk localturinggihan@turing.ce.pdn.ac.lk:/storage/scratch/e14-4yp-low-light-images/logging/turing/* ../../reports/server-gpu-util/logs/;
+scp -J e14158@tesla.ce.pdn.ac.lk servermonitoring@turing.ce.pdn.ac.lk:/localhome/servermonitoring/logging/turing/* ../../reports/server-gpu-util/logs/;
 scp -J e14158@tesla.ce.pdn.ac.lk gihan@kepler.ce.pdn.ac.lk:~/logging/kepler/* ../../reports/server-gpu-util/logs/;
 scp -J e14158@tesla.ce.pdn.ac.lk gihan@ai4covid.ee.pdn.ac.lk:~/logging/ai4covid/* ../../reports/server-gpu-util/logs/;
 # scp -J e14158@tesla.ce.pdn.ac.lk servermonitoring@ai4covid.ee.pdn.ac.lk:~/logging/ai4covid/* ../../reports/server-gpu-util/logs/;
