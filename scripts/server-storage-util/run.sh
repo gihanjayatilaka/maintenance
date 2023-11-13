a61cd3ed6dcd7e5f665703707c1ebf0e40198fa7
gihanjayatilaka <gihan@umd.edu>
2023-11-12 20:19:59 -0500

    update

--- scripts/server-storage-util/run.sh
+++ scripts/server-storage-util/run.sh
@@ -4,7 +4,7 @@
 #sudo du -hs --threshold=10G /storage/projects1/*;\
 #sudo du -hs --threshold=10G /storage/projects2/*;\
 #) | sort -hr > temp.txt
-#./download-logs.sh
+./download-logs.sh
 python gen-report.py -o ../../reports/server-storage-util/index.html
 echo "Done"
 
